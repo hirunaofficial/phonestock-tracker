4d40cb089e321d619f58db5f1e979d134bf518d7
GD Hiruna <hirunaofficial@gmail.com>
2024-06-02 11:44:45 +0530

    Add icon for the application window and disable window resize.

--- src/main/java/dev/hiruna/phone/PSTApplication.java
+++ src/main/java/dev/hiruna/phone/PSTApplication.java
@@ -3,6 +3,7 @@ package dev.hiruna.phone;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,15 @@ public class PSTApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(PSTApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+
+        // Set the icon of the application window
+        stage.getIcons().add(new Image(PSTApplication.class.getResourceAsStream("assets/icon.png")));
+
+        // Disable window resize
+        stage.setResizable(false);
+
+        stage.setTitle("PhoneStock Tracker (Track Every Phone, Every Time)");
         stage.setScene(scene);
         stage.show();
     }
