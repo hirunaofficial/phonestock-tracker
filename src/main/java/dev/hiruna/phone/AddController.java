5bc70ed27a1c6ebc713cde59c897c8a8375421a8
GD Hiruna <hirunaofficial@gmail.com>
2024-06-02 21:37:02 +0530

    Add btnBack function to AddController

--- src/main/java/dev/hiruna/phone/AddController.java
+++ src/main/java/dev/hiruna/phone/AddController.java
@@ -2,9 +2,15 @@ package dev.hiruna.phone;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AddController {
 
@@ -33,12 +39,19 @@ public class AddController {
     private TextField txtStorage;
 
     @FXML
-    void btnAddPhone(ActionEvent event) {
-
+    void btnBack(MouseEvent event) throws IOException {
+        Stage stage = (Stage) this.root.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("assets/icon.png"))); // Set the icon of the application window
+        stage.setResizable(false); // Disable window resize
+        stage.setTitle("PhoneStock Tracker (Track Every Phone, Every Time)");
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void btnBack(MouseEvent event) {
+    void btnAddPhone(ActionEvent event) {
 
     }
 
