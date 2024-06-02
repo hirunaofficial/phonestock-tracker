86f547fa2583245dee3e4e6d00e2728714acd68a
GD Hiruna <hirunaofficial@gmail.com>
2024-06-02 17:24:59 +0530

    Create search-view.fxml and SearchController

---
+++ src/main/java/dev/hiruna/phone/SearchController.java
@@ -0,0 +1,36 @@
+package dev.hiruna.phone;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SearchController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    void btnBack(MouseEvent event) throws IOException {
+        Stage stage = (Stage) this.root.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(MainApplication.class.getResourceAsStream("assets/icon.png"))); // Set the icon of the application window
+        stage.setResizable(false); // Disable window resize
+        stage.setTitle("Search - PhoneStock Tracker (Track Every Phone, Every Time)");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void btnDeletePhone(ActionEvent event) {
+
+    }
+
+}
