e2470b9946e3d4db03fd4e3da7b5de43143477b5
GD Hiruna <hirunaofficial@gmail.com>
2024-06-02 21:31:58 +0530

    Create AddController

---
+++ src/main/java/dev/hiruna/phone/AddController.java
@@ -0,0 +1,45 @@
+package dev.hiruna.phone;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class AddController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TextField txtBrand;
+
+    @FXML
+    private TextField txtColor;
+
+    @FXML
+    private TextField txtModel;
+
+    @FXML
+    private TextField txtPrice;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    private TextField txtRam;
+
+    @FXML
+    private TextField txtStorage;
+
+    @FXML
+    void btnAddPhone(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBack(MouseEvent event) {
+
+    }
+
+}
