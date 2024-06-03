76b8b36aaabb2047d3f5856cd8468827a49eb249
GD Hiruna <hirunaofficial@gmail.com>
2024-06-03 22:06:00 +0530

    Update UpdateFormController

--- src/main/java/dev/hiruna/phone/UpdateFormController.java
+++ src/main/java/dev/hiruna/phone/UpdateFormController.java
@@ -2,16 +2,11 @@ package dev.hiruna.phone;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
